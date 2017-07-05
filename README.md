1363fc968778dde3cc8cf24b561319bdb8c90379
Osku Pönkä <osku.ponka@hotmail.com>
2017-07-05 13:00:04 +0300

    Initial create-react-app commit with changes to file structure.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # internal-react-charts
-Charting with React. 
+Charting with React.
+
+npm install - to install dependencies.
+
+npm run start - builds the css and starts local server.
+
+npm run watch - builds and watches changes to scss files. 
---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2 className="header-text">Chart examples with Recharts</h2>
+        </div>
+        <p className="App-intro">
+          placeholder for charts
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
